248de6d8271ffc3ed4eaba950a99743d40867b81
sun <>
2024-08-16 13:43:58 +0800

    網頁list

--- src/App.vue
+++ src/App.vue
@@ -116,31 +116,30 @@
         </div>
 
         <div class="experience-list mt-20">
-          <div class="experience-item flex space-x-4 bg-gray-800 p-4 rounded-lg mb-8">
-          <img src="./assets/img/image/ocare.png" alt="Project Image" class="w-24 h-24 rounded-md">
-          <div>
-              <h3 class="text-white text-lg font-semibold">CaCa Taxi</h3>
-              <p class="text-gray-400 text-sm">2023.08 — 2024.02</p>
-              <p class="text-gray-400 text-sm mt-2">
-                  和團隊一起完成的叫車App，負責的部分是flutter App 雙平台，配合後端串接API，google地圖，還有司機端CaCa Driver 是兩個互相配合串接的App，類似Uber，持續維護中。
-              </p>
-              <div class="flex space-x-2 mt-2">
-                  <span class="badge">Flutter</span>
-                  <span class="badge">google地圖</span>
-                  <span class="badge">API</span>
-              </div>
-              <div class="ml-auto">
-              <a href="#" class="text-right text-white hover:text-gray-400 transition flex items-center mt-4">
-                  前往查看
-                  <svg class="w-4 h-4 ml-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path d="M10.5 17l6-6-6-6v12z"></path>
-                  </svg>
-              </a>
-              </div>
+          <div class="experience-item flex space-x-4 bg-gray-800 p-4 rounded-lg mb-8"> 
+            <img src="./assets/img/image/ocare.png" alt="Project Image" class="w-24 h-24 rounded-md">
+            <div>
+                <h3 class="text-white text-lg font-semibold">CaCa Taxi</h3>
+                <p class="text-gray-400 text-sm">2023.08 — 2024.02</p>
+                <p class="text-gray-400 text-sm mt-2">
+                    和團隊一起完成的叫車App，負責的部分是flutter App 雙平台，配合後端串接API，google地圖，還有司機端CaCa Driver 是兩個互相配合串接的App，類似Uber，持續維護中。
+                </p>
+                <div class="flex space-x-2 mt-2">
+                    <span class="badge">Flutter</span>
+                    <span class="badge">google地圖</span>
+                    <span class="badge">API</span>
+                </div>
+                <div class="flex mt-4">
+                  <a href="#" class="ml-auto text-right text-white hover:text-gray-400 transition flex items-center ">
+                      前往查看
+                      <svg class="w-4 h-4 ml-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                          <path d="M10.5 17l6-6-6-6v12z"></path>
+                      </svg>
+                  </a>
+                </div>
+            </div>
           </div>
-          
         </div>
-      </div>
 
     </main>
   </div>
